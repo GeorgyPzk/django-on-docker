ab54ef4022b3f4767f65d084373291b9e9afc0b2
georgy <georgy.puzakov@gmail.com>
2023-08-06 21:01:38 +0000

    Worcing Hello world! create views.py and edit urls.py

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin # old
+from django.urls import path # old
+
+from .views import helloView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', helloView, name='hello')
 ]
+
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#]
